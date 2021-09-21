b9c87a5276c4359b6a0d37919649b0e8f482b6c8
HassanAfnan <hassanafnan20@gmail.com>
2021-09-21 14:33:41 +0500

    deploy 3

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q7uub2h5_5-ax0)b0eu7c_rhc#hq_vw#%n(a7c5j^8b)__aq8w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
